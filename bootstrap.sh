5678bb3b9a1fcb6a150abfa5b0a8d1723cf7d08e
Richard Yen <richard.yen@enterprisedb.com>
2022-06-07 00:56:01 -0700

    add paths to virtual envs

--- bootstrap.sh
+++ bootstrap.sh
@@ -46,6 +46,8 @@ for file in ~/.{path,bash_prompt,exports,aliases,functions,extra}; do
   [ -r "\$file" ] && [ -f "\$file" ] && source "\$file";
 done;
 unset file;
+
+export PATH="/opt/EDB/TPA/bin:$HOME/.local/bin:$HOME/.poetry/bin:$PATH"
 __EOF__
   source ~/.bash_profile;
   installDocker
