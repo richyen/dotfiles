43d0d60dd062cb4bb2a1bc9bab132a3c0a7899d6
Richard Yen <richard.yen@enterprisedb.com>
2022-06-07 00:51:48 -0700

    add steps to upgrade python and install docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,33 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
+function installPython() {
+  sudo apt install python3.8
+  sudo apt install python3.8-distutils
+
+  wget https://bootstrap.pypa.io/get-pip.py
+  sudo python3.8 get-pip.py
+  rm get-pip.py
+}
+
+function installDocker() {
+  sudo apt-get update
+  sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo groupadd -aG docker ubuntu
+
+}
+
 function doIt() {
   cp .functions ~
   cp .aliases ~
@@ -21,6 +48,8 @@ done;
 unset file;
 __EOF__
   source ~/.bash_profile;
+  installDocker
+  installPython
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -33,8 +62,3 @@ else
   fi;
 fi;
 unset doIt;
-
-echo "Done.  You may also need to do the following:"
-echo "  - Install Docker (https://docs.docker.com/engine/install/ubuntu/)"
-echo "  - Set passwordless sudo"
-echo "  - Clone any internal repos"
