af5cb849b7972d78f500364ece2ffdf04f66d47d
Richard Yen <richard.yen@enterprisedb.com>
2022-06-07 09:17:16 -0700

    copy local stuff and clean up

--- provision.sh
+++ provision.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 scp ~/.ssh/id_rsa ${1}:~/.ssh/
+scp ~/.exports_local ${1}:~/
 ssh ${1} "git clone https://github.com/richyen/dotfiles.git"
 ssh ${1} "~/dotfiles/bootstrap.sh --force"
 ssh ${1} "~/dotfiles/github.sh"
+ssh ${1} "rm -rf ~/dotfiles"
 
 echo "Done.  You may also need to:"
 echo "  - Create symlink for TPA"
