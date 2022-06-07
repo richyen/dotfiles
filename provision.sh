0e3b62c6ce2339c64b121d26a0b15305b959b30b
Richard Yen <richard.yen@enterprisedb.com>
2022-06-07 01:22:01 -0700

    reminders to self

--- provision.sh
+++ provision.sh
@@ -4,3 +4,7 @@ scp ~/.ssh/id_rsa ${1}:~/.ssh/
 ssh ${1} "git clone https://github.com/richyen/dotfiles.git"
 ssh ${1} "~/dotfiles/bootstrap.sh --force"
 ssh ${1} "~/dotfiles/github.sh"
+
+echo "Done.  You may also need to:"
+echo "  - Create symlink for TPA"
+echo "  - Setup TPA"
