ff56577c0aa070e8a17a9e5fdad713f4253230f5
Richard Yen <richyen3@gmail.com>
2023-03-14 12:21:22 -0700

    use correct command

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,9 +27,9 @@ function installDocker() {
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-  sudo groupadd -aG docker ubuntu
+  sudo apt -y update
+  sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo usermod -aG docker ubuntu
 
 }
 
