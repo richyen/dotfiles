82c2c12437afedecae3a99c2738ba8ad40b5b44e
Richard Yen <richyen3@gmail.com>
2023-05-10 13:56:17 -0700

    add ssh-agent to non-mac installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -73,6 +73,29 @@ for file in ~/.{path,bash_prompt,exports,aliases,functions,extra}; do
 done;
 unset file;
 
+SSH_ENV="\$HOME/.ssh/agent-environment"
+
+function start_agent {
+    echo "Initialising new SSH agent..."
+    ssh-agent | sed 's/^echo/#echo/' > "\${SSH_ENV}"
+    echo succeeded
+    chmod 600 "\${SSH_ENV}"
+    . "\${SSH_ENV}" > /dev/null
+    ssh-add;
+}
+
+# Source SSH settings, if applicable
+
+if [ -f "\${SSH_ENV}" ]; then
+    . "\${SSH_ENV}" > /dev/null
+    ps -ef | grep \${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+        start_agent;
+    }
+else
+    start_agent;
+fi
+
+
 export PATH="/opt/EDB/TPA/bin:$HOME/.local/bin:$HOME/.poetry/bin:$PATH"
 __EOF__
   source ~/.bash_profile;
